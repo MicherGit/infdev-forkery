470f379d96c9c7cb4400676175e5c062429e08d6
MicherGit <40839581+MicherGit@users.noreply.github.com>
2021-02-05 08:31:27 -0500

    Initial commit

---
+++ src/main/java/net/textilemc/examplemod/mixin/ExampleMixin.java
@@ -0,0 +1,20 @@
+package net.textilemc.examplemod.mixin;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public abstract class ExampleMixin {
+
+    @Shadow
+    private String field_1103;
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void onInit(CallbackInfo ci) {
+        field_1103 = "Infdev 227 woop!!";
+    }
+}
